ad87398977b10f8c98a0a2817d3f96d53a61b2ae
Yashirizu <118454337+Yashirizu@users.noreply.github.com>
2022-11-22 17:56:56 +0100

    Add files via upload

---
+++ Exam_pyhton_Mathieu_Chartier.py
@@ -0,0 +1,58 @@
+##1 permuter deux valeurs d'un même tableau
+#myTables1 = [64,33,15,3,42]
+#
+#stockage=myTables1[1]
+#myTables1 [1]=myTables1[0]
+#myTables1 [0]= stockage
+#print (myTables1)
+#FONCTIONNE
+
+
+
+#2
+#myTables2 = [64,33,15,6,42]
+
+#for j in range (len(myTables2)):
+#    valMin=100
+#
+#    for i in range(j,len(myTables2)):
+#        if(myTables2[i]<(valMin)):
+#            valMin=myTables2[i]
+#            indiceMin= [i]
+#        save= myTables2[j]
+#        myTables2[j] = myTables2[indiceMin]
+#        myTables2[indiceMin]=save
+#print (myTables2)
+
+#3
+#myTables3=[64,33,15,6,42]
+#
+#for j in range(len(myTables3)):
+#    valMin=100
+#
+#    for i in range(j,len(myTables3)):
+#        if(myTables3[i])<=(valMin):
+#            valMin=myTables3[i]
+#            indiceMin=[i]
+#        save=myTables3[j]
+#        myTables3[j]=myTables3
+#        myTables3=myTables3[indiceMin]
+#stockage=myTables3[i]
+#myTables3 [i]=myTables3[i-1]
+#myTables3 [i-1]= stockage
+
+
+
+#4 Le tri à bulles considéré comme très lent parce le programme 
+#vérifie un par par chaque donnée et les remplaces au fur et à mesure.
+
+
+
+
+
+
+
+
+
+
+
